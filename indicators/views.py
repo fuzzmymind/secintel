f56c950bef10ed891f89e089f981918288e8d2a2
Manish Menon <manishmenon@Manishs-MBP.attlocal.net>
2020-04-11 21:18:05 -0400

    url vt complete

--- indicators/views.py
+++ indicators/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from .forms import AnalyzeUrlForm
+import pip._vendor.requests as requests
 import json
+import os
 
 def url_home(request):
     # if request.method == "GET":
@@ -11,8 +13,15 @@ def url_home(request):
 
 def url_analyze(request):
     url = request.POST["url"]
-    result = {
-        "key": url
+    api_key = os.getenv('VT_API_KEY')
+    headers = {
+        "x-apikey": api_key
     }
+    result = None
+    r = requests.get('https://www.virustotal.com/api/v3/urls/'+ url, headers=headers)
+    if r.status_code == 200: 
+        result = r.json()
+    else:
+        result = "No data found"
     return HttpResponse(json.dumps(result), content_type="application/json"
 )
